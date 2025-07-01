3690061facc8445603d3663b582cb1fe9baad94d
mihaid1994 <148491023+mihaid1994@users.noreply.github.com>
2025-07-01 10:52:24 +0500

    гугл ауф

--- js/auth.js
+++ js/auth.js
@@ -112,7 +112,7 @@ async function signInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin,
+        redirectTo: "https://maxdance.netlify.app", // ← ИЗМЕНИТЬ ЭТО
       },
     });
 
